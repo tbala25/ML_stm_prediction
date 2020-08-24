b0f00cef4fc65cbe66a313a325c6dacc23aab914
tbala25 <tbala25@users.noreply.github.com>
2020-08-23 21:17:59 -0500

    Data Extraction queries complete

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,5 +1,6 @@
 from localLibrary_DatabaseSchema import *
-from localLibrary_dataQueries.py import *
+from localLibrary_dataQueries import *
+import pandas as pd
 
 STM_queries = {'SG_STM':SG_STM,'MK_STM':MK_STM, 'YZ_STM':YZ_STM,
                 'FTS_STM':FTS_STM, 'CRM_STM':CRM_STM}
@@ -9,12 +10,32 @@ nonSTM_queries = {'SG_nonSTM':SG_nonSTM,'MK_nonSTM':MK_nonSTM,
 lost_queries = {'SG_lost':SG_lost,'MK_lost':MK_lost, 'YZ_lost':YZ_lost,
                 'FTS_lost':FTS_lost, 'CRM_lost':CRM_lost}
 
-for k,query in STM_queries:
-    cursor.executeStament(query)
+for query in STM_queries:
+    cursor.execute(STM_queries[query])
     df_columns = [column[0] for column in cursor.description]
     df_data = cursor.fetchall()
     df_arr = []
     for row in df_data:
         df_arr.append(dict(zip(df_columns, row)))
     df = pd.DataFrame(df_arr, columns = df_columns)
-    df.to_csv(k+'.csv')
+    df.to_csv('data/' + query + '.csv', index = False)
+
+for query in nonSTM_queries:
+    cursor.execute(nonSTM_queries[query])
+    df_columns = [column[0] for column in cursor.description]
+    df_data = cursor.fetchall()
+    df_arr = []
+    for row in df_data:
+        df_arr.append(dict(zip(df_columns, row)))
+    df = pd.DataFrame(df_arr, columns = df_columns)
+    df.to_csv('data/' + query + '.csv', index = False)
+
+for query in lost_queries:
+    cursor.execute(lost_queries[query])
+    df_columns = [column[0] for column in cursor.description]
+    df_data = cursor.fetchall()
+    df_arr = []
+    for row in df_data:
+        df_arr.append(dict(zip(df_columns, row)))
+    df = pd.DataFrame(df_arr, columns = df_columns)
+    df.to_csv('data/' + query + '.csv', index = False)
